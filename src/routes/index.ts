ae744a567259e114640740f486169c3452c98660
Dante05S <Dante05S@hotmail.com>
2023-07-28 03:19:29 -0500

    :gear: add response http

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
+import { ResponseCode, http } from '../helpers/request'
 
 const router = Router()
 
 router.get('/', (_req, res) => {
-  res.json({ data: null, code: 200 })
+  res.json(http.response(null, ResponseCode.OK, 'Welcome to chat-core!'))
 })
 
 export default router
